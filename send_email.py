9f61d7f0f3a602ca95521b3cbbf9421dd2057e51
Suvamp <patelsuvam621@gmail.com>
2023-07-18 14:27:47 -0700

    Send an email with news titles and descriptions

---
+++ send_email.py
@@ -0,0 +1,15 @@
+import smtplib, ssl
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    user_email = "suvamp@uci.edu"
+    password = "rijqzxgaqqhskoof"
+
+    receiver = "suvamp@uci.edu"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(user_email, password)
+        server.sendmail(user_email, receiver, message)
